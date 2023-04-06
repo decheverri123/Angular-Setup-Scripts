9aaf78e719e8fdd763637cae45a1b546ae994442
Daniel Betancur-Echeverri <decheverri123@gmail.com>
2023-04-06 09:40:38 -0400

    Add files via upload

--- create-ang-tail-app.sh
+++ create-ang-tail-app.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# Check if a project name is provided
-if [ -z "$1" ]
-then
+# Check if a project name is provided and set the project name
+if [ -z "$1" ]; then
   echo "Please provide a project name"
   exit 1
+else
+  PROJECT_NAME=$1
 fi
 
-# Set the project name
-PROJECT_NAME=$1
-
-# Create a new Angular project
-ng new $PROJECT_NAME --style=css --routing=true
-cd $PROJECT_NAME
+# Create a new Angular project and navigate to the project directory
+ng new $PROJECT_NAME --style=css --routing=true && cd $PROJECT_NAME
 
 # Install Tailwind CSS and dependencies
-npm install -D tailwindcss postcss autoprefixer
-npx tailwindcss init
-npm i daisyui
+npm install -D tailwindcss postcss autoprefixer daisyui
 
-# Create a tailwind.config.js file
-cat << EOF > tailwind.config.js
+# Initialize Tailwind CSS and create a tailwind.config.js file
+npx tailwindcss init -p
+cat > tailwind.config.js <<- EOF
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{html,ts}"],
+  theme: { extend: {} },
   plugins: [require("daisyui")],
 }
 EOF
@@ -35,6 +27,5 @@ EOF
 # Update src/styles.scss
 echo -e "@import 'tailwindcss/base';\n@import 'tailwindcss/components';\n@import 'tailwindcss/utilities';" >> src/styles.css
 
-echo "Project setup complete. To start, run:"
-echo "cd $PROJECT_NAME"
-echo "ng serve"
+# Instructions for starting the project
+echo -e "Project setup complete. To start, run:\ncd $PROJECT_NAME\nng serve"
--- create-tauri-ang-app.sh
+++ create-tauri-ang-app.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# Check if a project name is provided
-if [ -z "$1" ]
-then
+# Check if a project name is provided and set the project name
+if [ -z "$1" ]; then
   echo "Please provide a project name"
   exit 1
+else
+  PROJECT_NAME=$1
 fi
 
-# Set the project name
-PROJECT_NAME=$1
+# Create a new Angular project and navigate to the project directory
+ng new $PROJECT_NAME --style=css --routing=true && cd $PROJECT_NAME
 
-# Create a new Angular project
-ng new $PROJECT_NAME --style=css --routing=true
-cd $PROJECT_NAME
+# Install Tailwind CSS, Tauri dependencies, and other required packages
+npm install -D tailwindcss postcss autoprefixer daisyui @tauri-apps/cli @tauri-apps/api
 
-# Install Tailwind CSS and dependencies
-npm install -D tailwindcss postcss autoprefixer
-npx tailwindcss init
-npm i daisyui
-
-# Create a tailwind.config.js file
-cat << EOF > tailwind.config.js
+# Initialize Tailwind CSS and create a tailwind.config.js file
+npx tailwindcss init -p
+cat > tailwind.config.js <<- EOF
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{html,ts}"],
+  theme: { extend: {} },
   plugins: [require("daisyui")],
 }
 EOF
@@ -35,14 +27,11 @@ EOF
 # Update src/styles.scss
 echo -e "@import 'tailwindcss/base';\n@import 'tailwindcss/components';\n@import 'tailwindcss/utilities';" >> src/styles.css
 
-# Install Tauri dependencies and CLI
-npm install -D @tauri-apps/cli
-npm install @tauri-apps/api
+# Initialize Tauri
 npx tauri init
 
 # Add the "tauri:tauri" script to the package.json file
 jq '.scripts += {"tauri": "tauri"}' package.json > package.json.tmp && mv package.json.tmp package.json
 
-echo "Project setup complete. To start, run:"
-echo "cd $PROJECT_NAME"
-echo "npm run tauri dev"
+# Instructions for starting the project
+echo -e "Project setup complete. To start, run:\ncd $PROJECT_NAME\nnpm run tauri dev"
